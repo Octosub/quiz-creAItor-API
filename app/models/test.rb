ac07aa02379ab3fceebfb6884ae86d4e75f1f7ec
Noah Endo <noah.eyeing@gmail.com>
2024-03-21 17:45:51 +0900

    structure fixed

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
 
-  def create_test(number_of_challenges, max_score, time, input)
+  def create_test(number_of_challenges, input)
     client = OpenAI::Client.new
 
     response = client.chat(
@@ -11,31 +11,25 @@ class Test < ApplicationRecord
           "
           Generate the following json object and fill out the curly braces with the instructions inside the curly braces. Take the following topic to generate the questions and answers '#{input}' and create #{number_of_challenges} challenges:
           {
-          'test': {
-            'max_score': '#{max_score}',
-            'time': '#{time}',
-            'challenges': {
-              'challenge1': {
-                'question': '{generate question 1}',
-                'choices': {
-                  'choice1': '{generate choice 1}',
-                  'choice2': '{generate choice 2}',
-                  'choice3': '{generate choice 3}',
-                  'choice4': '{generate choice 4}'
-                },
-                'answer' : '{generate answer 1}'
-              },
-              'challenge2' : {
-                'question': '{generate question 2}',
-                'choices': {
-                  'choice1': '{generate choice 1}',
-                  'choice2': '{generate choice 2}',
-                  'choice3': '{generate choice 3}',
-                  'choice4': '{generate choice 4}'
-                },
-                'answer': '{generate answer 2}'
-              }
-            }
+          'challenge1': {
+            'question': '{generate question 1}',
+            'choices': {
+              'choice1': '{generate choice 1}',
+              'choice2': '{generate choice 2}',
+              'choice3': '{generate choice 3}',
+              'choice4': '{generate choice 4}'
+            },
+            'answer' : '{generate answer 1}'
+          },
+          'challenge2' : {
+            'question': '{generate question 2}',
+            'choices': {
+              'choice1': '{generate choice 1}',
+              'choice2': '{generate choice 2}',
+              'choice3': '{generate choice 3}',
+              'choice4': '{generate choice 4}'
+            },
+            'answer': '{generate answer 2}'
           }
           Dont put any '\' or '\n' in the json object. The json object should be in one line.
           "}],
