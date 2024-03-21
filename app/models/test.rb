f50a23ea379deb08bb538797638d7e93b2fcd1ab
Noah Endo <noah.eyeing@gmail.com>
2024-03-21 17:12:03 +0900

    gpt creates tests but wrong format

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,17 +1,47 @@
 class Test < ApplicationRecord
 
-  def chatgpt
+  def create_test(number_of_challenges, max_score, time, input)
     client = OpenAI::Client.new
 
     response = client.chat(
       parameters: {
           model: "gpt-3.5-turbo",
           response_format: { type: "json_object" },
-          messages: [{ role: "user", content: "Hello!"}],
+          messages: [{ role: "user", content:
+          "
+          Generate the following json object and fill out the curly braces with the instructions inside the curly braces. Take the following topic to generate the questions and answers '#{input}' and create #{number_of_challenges} challenges:
+          {
+          'test': {
+            'max_score': '#{max_score}',
+            'time': '#{time}',
+            'challenges': {
+              'challenge1': {
+                'question': '{generate question 1}',
+                'choices': {
+                  'choice1': '{generate choice 1}',
+                  'choice2': '{generate choice 2}',
+                  'choice3': '{generate choice 3}',
+                  'choice4': '{generate choice 4}'
+                },
+                'answer' : '{generate answer 1}'
+              },
+              'challenge2' : {
+                'question': '{generate question 2}',
+                'choices': {
+                  'choice1': '{generate choice 1}',
+                  'choice2': '{generate choice 2}',
+                  'choice3': '{generate choice 3}',
+                  'choice4': '{generate choice 4}'
+                },
+                'answer': '{generate answer 2}'
+              }
+            }
+          }
+          "}],
           temperature: 0.7,
       }
     )
 
-    self.challenges["challenge1"]["answer"] = response["choices"][0]["message"]["content"]
+    self.challenges["challenge1"]["answer"] = response.dig("choices", 0, "message", "content")
   end
 end
