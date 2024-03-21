dce23ace69e1119984dda8099d7906da95d14404
Noah Endo <noah.eyeing@gmail.com>
2024-03-21 15:55:43 +0900

    new version

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,6 +6,7 @@ class Test < ApplicationRecord
     response = client.chat(
       parameters: {
           model: "gpt-3.5-turbo",
+          response_format: { type: "json_object" },
           messages: [{ role: "user", content: "Hello!"}],
           temperature: 0.7,
       }
